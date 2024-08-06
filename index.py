58c71a5b2f76d2acbb3a3a4a7c2dc1d1c3fef9e6
Josué Juca <josuejuca19@gmail.com>
2024-08-06 15:54:41 -0300

    [ 2 ] - SEFAZ / RECEITA / ELEITORAL / CRIMINAL / CIVEL

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, send_file
 import os
 import pdfplumber
 import re
+import pandas as pd
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads/'
@@ -28,7 +30,17 @@ def upload_files():
             info['filename'] = file.filename
             extracted_info.append(info)
     
-    return render_template('result.html', extracted_info=extracted_info)
+    # Save to Excel
+    df = pd.DataFrame(extracted_info)
+    filename = f'avaliacao-{datetime.today().strftime("%d-%m-%Y")}.xlsx'
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    df.to_excel(filepath, index=False)
+
+    return render_template('result.html', extracted_info=extracted_info, filename=filename)
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), as_attachment=True)
 
 def extract_text_from_pdf(pdf_path):
     text = ""
@@ -40,15 +52,32 @@ def extract_text_from_pdf(pdf_path):
     return text
 
 def extract_important_info(text):
+    if "CERTIDÃO JUDICIAL" in text:
+        return extract_judicial_cert_info(text)
+    elif "CERTIDÃO NEGATIVA DE DÉBITOS" in text or "CERTIDÃO POSITIVA DE DÉBITOS COM EFEITO DE NEGATIVA" in text:
+        return extract_sefaz_cert_info(text)
+    elif "CERTIDÃO NEGATIVA DE DÉBITOS RELATIVOS AOS TRIBUTOS FEDERAIS" in text or "CERTIDÃO POSITIVA COM EFEITOS DE NEGATIVA DE DÉBITOS RELATIVOS AOS TRIBUTOS" in text:
+        return extract_receita_cert_info(text)
+    else:
+        return {
+            'certidao': 'Tipo de certidão não reconhecido',
+            'status': 'Não encontrado',
+            'nome': 'Não encontrado',
+            'cpf': 'Não encontrado',
+            'descricao': 'Não aplicável'
+        }
+
+def extract_judicial_cert_info(text):
     info = {}
-    
+
     # Nome da certidão
     certidao_start = text.find("CERTIDÃO JUDICIAL")
     if certidao_start != -1:
         certidao_start += len("CERTIDÃO JUDICIAL")
         certidao_end = text.find("/", certidao_start)
         if certidao_end != -1:
-            info['certidao'] = text[certidao_start:certidao_end].strip()
+            certidao_nome = text[certidao_start:certidao_end].strip()
+            info['certidao'] = map_certidao_name(certidao_nome)
         else:
             info['certidao'] = 'Não encontrado'
     else:
@@ -85,7 +114,133 @@ def extract_important_info(text):
     else:
         info['cpf'] = 'Não encontrado'
     
+    # Descrição
+    info['descricao'] = 'Não aplicável'
+    
     return info
 
+def extract_receita_cert_info(text):
+    info = {}
+
+    if "MINISTÉRIO DA FAZENDA" in text:
+        info['certidao'] = "RECEITA"
+
+    
+    # Status da certidão
+    if "não constam pendências em seu nome" in text:
+        info['status'] = "NÃO CONSTAM DÉBITOS"
+    elif "constam débitos" in text:
+        info['status'] = "HÁ DÉBITOS"
+    else:
+        info['status'] = 'Não encontrado'
+    
+    
+    # Nome da pessoa
+    nome_start = text.find("Nome: ")
+    if nome_start != -1:
+        nome_start += len("Nome: ")
+        nome_end = text.find("\nCPF:", nome_start)
+        if nome_end != -1:
+            info['nome'] = text[nome_start:nome_end].strip().upper()
+        else:
+            info['nome'] = text[nome_start:].strip().upper()
+    else:
+        info['nome'] = 'Não encontrado'
+    
+    # CPF da pessoa
+    cpf_pattern = r"CPF:\s*(\d{3}\.\d{3}\.\d{3}-\d{2})"
+    match = re.search(cpf_pattern, text)
+    if match:
+        info['cpf'] = match.group(1)
+    else:
+        info['cpf'] = 'Não encontrado'
+    
+    # Descrição de pendência
+    #info['descricao'] = 'Não aplicável'
+    # Descrição de pendência
+    descricao_start = text.find("é certificado que:")
+    if descricao_start != -1:
+        descricao_start += len("é certificado que:")
+        descricao_end = text.find("Conforme disposto nos arts", descricao_start)
+        if descricao_end != -1:
+            info['descricao'] = text[descricao_start:descricao_end].strip()
+        else:
+            info['descricao'] = 'SEFAZ - SEM DÉBITOS'
+    else:
+        info['descricao'] = 'Não aplicável'
+    
+    
+    return info
+
+def extract_sefaz_cert_info(text):
+    info = {}
+
+    # Verifica se "MINISTÉRIO DA FAZENDA" está presente
+    if "MINISTÉRIO DA FAZENDA" in text:
+        return extract_receita_cert_info(text)
+    
+    # Nome da certidão
+    if "CERTIDÃO NEGATIVA DE DÉBITOS" in text:
+        info['certidao'] = "SEFAZ"
+    elif "CERTIDÃO POSITIVA DE DÉBITOS COM EFEITO DE NEGATIVA" in text:
+        info['certidao'] = "SEFAZ"
+    else:
+        info['certidao'] = 'Não encontrado'
+    
+    # Status da certidão
+    if "Até esta data não constam débitos de tributos" in text:
+        info['status'] = "NÃO CONSTAM DÉBITOS"
+    elif "Pelos débitos acima responde solidariamente o adquirente" in text:
+        info['status'] = "HÁ DÉBITOS"
+    else:
+        info['status'] = 'Não encontrado'
+    
+    # Nome da pessoa
+    nome_start = text.find("NOME:")
+    if nome_start != -1:
+        nome_start += len("NOME:")
+        endereco_start = text.find("ENDEREÇO:", nome_start)
+        if endereco_start != -1:
+            info['nome'] = text[nome_start:endereco_start].strip().upper()
+        else:
+            info['nome'] = text[nome_start:].strip().upper()
+    else:
+        info['nome'] = 'Não encontrado'
+    
+    # CPF da pessoa
+    cpf_pattern = r"CPF:\s*(\d{3}\.\d{3}\.\d{3}-\d{2})"
+    match = re.search(cpf_pattern, text)
+    if match:
+        info['cpf'] = match.group(1)
+    else:
+        info['cpf'] = 'Não encontrado'
+    
+    # Descrição de pendência
+    descricao_start = text.find("CERTIFICAMOS QUE _____________________________")
+    if descricao_start != -1:
+        descricao_start += len("CERTIFICAMOS QUE _____________________________")
+        descricao_end = text.find("Pelos débitos acima responde solidariamente o adquirente", descricao_start)
+        if descricao_end != -1:
+            info['descricao'] = text[descricao_start:descricao_end].strip()
+        else:
+            info['descricao'] = 'SEFAZ - SEM DÉBITOS'
+    else:
+        info['descricao'] = 'Não aplicável'
+    
+    return info
+
+
+def map_certidao_name(certidao_nome):
+    if "PARA FINS ELEITORAIS" in certidao_nome:
+        return "ELEITORAL"
+    elif "CÍVEL" in certidao_nome:
+        return "CÍVEL"
+    elif "CRIMINAL" in certidao_nome:
+        return "CRIMINAL"
+    else:
+        return certidao_nome
+
 if __name__ == '__main__':
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
     app.run(debug=True)
